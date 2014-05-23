93cf0251c88519af71f88ce7cf012fde0b1b9b48
Zhuyi Xue (bcgsc) <alfred532008@gmail.com>
2014-05-23 10:24:03 -0700

    giblib 1.2.4 depends_on x11 only on mac

--- Library/Formula/giblib.rb
+++ Library/Formula/giblib.rb
@@ -5,7 +5,7 @@ class Giblib < Formula
   url 'http://linuxbrit.co.uk/downloads/giblib-1.2.4.tar.gz'
   sha1 '342e6f7882c67d2277e1765299e1be5078329ab0'
 
-  depends_on :x11
+  depends_on :x11 if OS.mac?
   depends_on 'imlib2' => :build
 
   def install
