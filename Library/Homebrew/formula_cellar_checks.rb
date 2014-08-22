7d8ddda2ddadd280f84d8bf9c73961862c9bac7c
Elliot Saba <staticfloat@gmail.com>
2014-08-22 11:37:09 -0700

    Ignore .so files with soname versioning builtin when linking kegs

--- Library/Homebrew/formula_cellar_checks.rb
+++ Library/Homebrew/formula_cellar_checks.rb
@@ -62,6 +62,7 @@ module FormulaCellarChecks
                           .jar .prl .pm .sh)
     non_libraries = f.lib.children.select do |g|
       next if g.directory?
+      next if /.*\.so[.\d]*/.match(g.basename)
       not valid_extensions.include? g.extname
     end
     return if non_libraries.empty?
