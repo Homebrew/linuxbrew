c449868d0fff6129b7f60a6d9b2d55e6c849ebbd
Jonas Weber <mail@jonasw.de>
2015-10-21 17:06:58 +0200

    Mark shared libraries executable as well
    Position Independent Executables (PIE) are not executables
    in a ELF sense, but shared objects. If a compiler (by system defaults)
    produces PIE, the resulting tools are not executable without this
    change.
    
    fixes #612

--- Library/Homebrew/cleaner.rb
+++ Library/Homebrew/cleaner.rb
@@ -87,7 +87,7 @@ class Cleaner
         path.unlink
       else
         # Set permissions for executables and non-executables
-        perms = if path.mach_o_executable? || path.text_executable?
+        perms = if path.mach_o_executable? || path.dylib? || path.text_executable?
           0555
         else
           0444
