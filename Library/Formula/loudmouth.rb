ad6f10fb50c00504cf0274e3970063834dd44c52
Jesse Hallett <jesse@sitr.us>
2015-11-14 16:56:00 -0800

    Ensures that loudmouth formula includes -lgobject-2.0 argument in linker command

--- Library/Formula/loudmouth.rb
+++ Library/Formula/loudmouth.rb
@@ -46,6 +46,7 @@ class Loudmouth < Formula
   end
 
   def install
+    ENV['LIBS'] = '-lgobject-2.0'
     system "./autogen.sh", "-n" if build.head?
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}", "--with-ssl=gnutls"
