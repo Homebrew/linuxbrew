cfed2dd2a523d0edb0373c82928e33c379ee5424
Tolmachev Arseny <eiennohito@gmail.com>
2016-02-25 18:50:15 +0900

    add homebrew default path to scons PATH variable

--- Library/Formula/scons.rb
+++ Library/Formula/scons.rb
@@ -13,6 +13,11 @@ class Scons < Formula
 
   def install
     man1.install gzip("scons-time.1", "scons.1", "sconsign.1")
+    inreplace(
+      "engine/SCons/Platform/posix.py",
+      "env['ENV']['PATH']    = '/usr/local/bin:/opt/bin:/bin:/usr/bin'",
+      "env['ENV']['PATH']    = '#{HOMEBREW_PREFIX/'bin'}:/usr/local/bin:/opt/bin:/bin:/usr/bin'"
+    )
     system "/usr/bin/python", "setup.py", "install",
              "--prefix=#{prefix}",
              "--standalone-lib",
